220f9a1c95a6ac99d33652a6ab45a554c83c39da
alexpung <31250276+alexpung@users.noreply.github.com>
2019-10-20 22:21:22 +0800

    Database prototype: Dump some of the data from xml to sqlite database

---
+++ importdata.py
@@ -0,0 +1,104 @@
+import sqlite3
+import xml.etree.ElementTree as ET
+
+
+def create_table():
+    with sqlite3.connect('account.db') as conn:
+        c = conn.cursor()
+        sql = """CREATE TABLE IF NOT EXISTS "SecurityInfo" (
+        "conid"	INTEGER,
+        "assetCategory"	TEXT,
+        "currency"	TEXT,
+        "description"	TEXT,
+        "symbol"	TEXT,
+        "underlyingCategory"	TEXT,
+        "underlyingSymbol"	TEXT,
+        PRIMARY KEY("conid")
+        );"""
+        c.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS "EquitySummaryByReportDateInBase" (
+        "reportDate"	TEXT,
+        "accountId"	TEXT,
+        "total"	REAL NOT NULL,
+        PRIMARY KEY("reportDate","accountId")
+        );"""
+        c.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS "StatementOfFundsLine" (
+        "transactionID"	TEXT,
+        "date"	TEXT,
+        "activityDescription"	TEXT,
+        "activityCode"	TEXT,
+        "amount"	REAL,
+        "conid"	INTEGER,
+        "currency"	TEXT,
+        "tradeCommission"	REAL,
+        "tradeID"	INTEGER,
+        "tradePrice"	REAL,
+        "tradeQuantity"	REAL,
+        "tradeTax"	REAL,
+        PRIMARY KEY("date","transactionID")
+        );"""
+        c.execute(sql)
+        sql = """CREATE TABLE IF NOT EXISTS "OpenPosition" (
+                "conid"	INTEGER,
+                "markPrice"	REAL,
+                "position" REAL,
+                PRIMARY KEY("conid")
+                );"""
+        c.execute(sql)
+
+
+def import_to_db(file):
+    with open(file, 'r') as f:
+        root = ET.fromstring(f.read())
+    with sqlite3.connect('account.db') as conn:
+        c = conn.cursor()
+        sql = "INSERT OR REPLACE INTO EquitySummaryByReportDateInBase VALUES (?, ?, ?)"
+        day_nav_list = [(x.attrib['reportDate'], x.attrib['accountId'], x.attrib['total'])
+                        for x in root.iter('EquitySummaryByReportDateInBase')]
+        c.executemany(sql, day_nav_list)
+        sql = "INSERT OR REPLACE INTO StatementOfFundsLine VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?, ?)"
+        sof_list = [(
+            x.attrib['transactionID'],
+            x.attrib['date'],
+            x.attrib['activityDescription'],
+            x.attrib['activityCode'],
+            x.attrib['amount'],
+            x.attrib['conid'],
+            x.attrib['currency'],
+            x.attrib['tradeCommission'],
+            x.attrib['tradeID'],
+            x.attrib['tradePrice'],
+            x.attrib['tradeQuantity'],
+            x.attrib['tradeTax']
+        ) for x in root.iter('StatementOfFundsLine')]
+        c.executemany(sql, sof_list)
+        sql = "INSERT OR REPLACE INTO SecurityInfo VALUES (?, ?, ?, ?, ?, ?, ?)"
+        sof_list = [(
+            x.attrib['conid'],
+            x.attrib['assetCategory'],
+            x.attrib['currency'],
+            x.attrib['description'],
+            x.attrib['symbol'],
+            x.attrib['underlyingCategory'],
+            x.attrib['underlyingSymbol']
+        ) for x in root.iter('SecurityInfo')]
+        c.executemany(sql, sof_list)
+        sql = "DELETE FROM OpenPosition"
+        c.execute(sql)
+        sql = "INSERT INTO OpenPosition VALUES (?, ?, ?)"
+        sof_list = [(
+            x.attrib['conid'],
+            x.attrib['markPrice'],
+            x.attrib['position']
+        ) for x in root.iter('OpenPosition')]
+        c.executemany(sql, sof_list)
+
+
+if __name__ == '__main__':
+    create_table()
+    # import_to_db('data2015.xml')
+    # import_to_db('data2016.xml')
+    # import_to_db('data2017.xml')
+    # import_to_db('data2018.xml')
+    import_to_db('data2019.xml')
