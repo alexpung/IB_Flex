f168df58f979769b0bce539607322d6b856a0de3
alexpung <31250276+alexpung@users.noreply.github.com>
2019-10-22 00:04:34 +0800

    refactor raw sql query to pandas code

--- app.py
+++ app.py
@@ -9,24 +9,27 @@ from dash.dependencies import Input, Output
 
 conn = sqlite3.connect("account.db")
 with conn:
-    df_equity = pd.read_sql_query("""SELECT reportDate, SUM(total) as total
-                                     FROM EquitySummaryByReportDateInBase
-                                     GROUP BY reportDate""", conn)
-    graph_list = [{'label': 'Account Value', 'value': 'account value'}]
-    df_future_list = pd.read_sql_query("""SELECT DISTINCT SecurityInfo.underlyingSymbol
-    FROM SecurityInfo
-    WHERE assetCategory = "FUT" """, conn)
-    graph_list.extend([{'label': x, 'value': x} for x in df_future_list['underlyingSymbol'].tolist()])
-    df_future_pnl = pd.read_sql_query("""SELECT underlyingSymbol, date, SUM(amount) OVER (PARTITION BY SecurityInfo.underlyingSymbol ORDER BY StatementOfFundsLine.date) as pnl
-        FROM StatementOfFundsLine
-        INNER JOIN SecurityInfo on StatementOfFundsLine.conid = SecurityInfo.conid
-        WHERE StatementOfFundsLine.conid in (
-        SELECT SecurityInfo.conid
-        FROM SecurityInfo 
-        WHERE assetCategory = "FUT")""", conn)
+    # Read all tables from database
+    df_equity = pd.read_sql_query("SELECT * FROM EquitySummaryByReportDateInBase", conn)
+    df_statement_of_fund = pd.read_sql_query("SELECT * FROM StatementOfFundsLine", conn)
+    df_open_position = pd.read_sql_query("SELECT * FROM OpenPosition", conn)
+    df_security_info = pd.read_sql_query("SELECT * FROM SecurityInfo", conn)
+# prepare data for graphing
 
+# This is the account value graph, values from multiple accounts are summed, as I have more than one account
+df_equity_sum = df_equity.groupby(['reportDate']).agg('sum').reset_index()
+# Get cumulative PnL for each future contract
+df_future_list = df_security_info[df_security_info.assetCategory == 'FUT'].underlyingSymbol.unique()
+df_sof_info = pd.merge(df_statement_of_fund, df_security_info, on='conid')
+df_future_pnl = df_sof_info[df_sof_info.assetCategory == 'FUT'][['underlyingSymbol', 'date', 'amount']]
+df_future_pnl['cumsum'] = df_future_pnl.groupby('underlyingSymbol').cumsum()
+
+# Prepare drop down list
+graph_list = [{'label': 'Account Value', 'value': 'account value'}]
+graph_list.extend([{'label': x, 'value': x} for x in df_future_list])
+
+# HTML layout
 app = dash.Dash(__name__)
-
 app.layout = html.Div(children=[
     dcc.Dropdown(
         id='future_symbol',
@@ -42,11 +45,11 @@ app.layout = html.Div(children=[
 )
 def update_graph(selected_contract):
     if selected_contract == 'account value':
-        traces = [go.Scatter(x=df_equity['reportDate'], y=df_equity['total'])]
+        traces = [go.Scatter(x=df_equity_sum['reportDate'], y=df_equity_sum['total'])]
         title = 'Account value'
     else:
         filtered_df = df_future_pnl[df_future_pnl.underlyingSymbol == selected_contract]
-        traces = [go.Scatter(x=filtered_df['date'], y=filtered_df['pnl'])]
+        traces = [go.Scatter(x=filtered_df['date'], y=filtered_df['cumsum'])]
         title = f'PnL for {selected_contract}'
     return {
         'data': traces,
