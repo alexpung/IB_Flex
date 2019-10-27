ce6e10c9266760ca183a4a45863602a715dd84d0
alexpung <31250276+alexpung@users.noreply.github.com>
2019-10-27 14:05:29 +0800

    use dash_bootstrap_components for navigation bar (new dependency) calculate profit and loss from deposit and withdrawal history

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import sqlite3
 
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
+
+from pages import futures, equity
 
 conn = sqlite3.connect("account.db")
 with conn:
@@ -18,39 +21,65 @@ with conn:
 
 # This is the account value graph, values from multiple accounts are summed, as I have more than one account
 df_equity_sum = df_equity.groupby(['reportDate']).agg('sum').reset_index()
-# Get cumulative PnL for each future contract
-df_future_list = df_security_info[df_security_info.assetCategory == 'FUT'].underlyingSymbol.unique()
+
+# Get statement of fund and open position with security information
 df_sof_info = pd.merge(df_statement_of_fund, df_security_info, on='conid')
+df_open_position_info = df_open_position.merge(df_security_info, on='conid')
+
+# Get cumulative PnL for each future contract
 df_future_pnl = df_sof_info[df_sof_info.assetCategory == 'FUT'][['underlyingSymbol', 'date', 'amount']]
-df_future_pnl['cumsum'] = df_future_pnl.groupby('underlyingSymbol').cumsum()
+df_future_pnl['Profit'] = df_future_pnl.groupby('underlyingSymbol').cumsum()
 
-# Prepare drop down list
-graph_list = [{'label': 'Account Value', 'value': 'account value'}]
-graph_list.extend([{'label': x, 'value': x} for x in df_future_list])
+df_future_open_position_info = df_open_position_info[df_open_position_info['assetCategory'] == 'FUT']
+df_future_table = df_future_pnl.sort_values('date').groupby('underlyingSymbol').tail(1)
+df_future_table_show = df_future_table.merge(df_future_open_position_info, how='left', on='underlyingSymbol')[[
+    'underlyingSymbol', 'position', 'Profit']].round(2).fillna('Closed')
 
+# get deposit and withdrawals
+df_deposit_withdrawal = df_statement_of_fund[(df_statement_of_fund['activityCode'] == 'DEP') |
+                                             (df_statement_of_fund['activityCode'] == 'WITH')]
+df_deposit_withdrawal['amountInBase'] = df_deposit_withdrawal['amount'] * df_deposit_withdrawal['fxRateToBase']
+df_deposit_withdrawal = df_deposit_withdrawal[['date', 'amountInBase']]
+df_equity_sum = df_equity_sum.merge(df_deposit_withdrawal, how='left', left_on='reportDate', right_on='date')
+df_equity_sum = df_equity_sum.drop(['date'], axis=1)
+df_equity_sum = df_equity_sum.fillna(0)
+df_equity_sum['Net total deposit and withdrawal'] = df_equity_sum['amountInBase'].cumsum()
+df_equity_sum['Profit and Loss'] = df_equity_sum['total'] - df_equity_sum['Net total deposit and withdrawal']
 # HTML layout
-app = dash.Dash(__name__)
-app.layout = html.Div(children=[
-    dcc.Dropdown(
-        id='future_symbol',
-        options=graph_list
-    ),
-    dcc.Graph(id='pnl_graph'),
-])
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP],
+    meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+app.config.suppress_callback_exceptions = True
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+)
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/future":
+        return futures.create_layout(df_future_table_show)
+    if pathname == "/equity history":
+        return equity.create_layout(df_equity_sum)
+    else:
+        return equity.create_layout(df_equity_sum)
 
 
 @app.callback(
     Output('pnl_graph', 'figure'),
-    [Input('future_symbol', 'value')]
+    [Input('future_summary_table', 'selected_rows')]
 )
-def update_graph(selected_contract):
-    if selected_contract == 'account value':
-        traces = [go.Scatter(x=df_equity_sum['reportDate'], y=df_equity_sum['total'])]
-        title = 'Account value'
+def update_graph(selected):
+    if selected is None:
+        traces = []
+        symbol_name = ""
     else:
-        filtered_df = df_future_pnl[df_future_pnl.underlyingSymbol == selected_contract]
-        traces = [go.Scatter(x=filtered_df['date'], y=filtered_df['cumsum'])]
-        title = f'PnL for {selected_contract}'
+        symbol_name = df_future_table_show.iloc[selected[0]]['underlyingSymbol']
+        filtered_df = df_future_pnl[df_future_pnl.underlyingSymbol == symbol_name]
+        traces = [go.Scatter(x=filtered_df['date'], y=filtered_df['Profit'])]
+    title = f'PnL for {symbol_name}'
     return {
         'data': traces,
         'layout': go.Layout(
